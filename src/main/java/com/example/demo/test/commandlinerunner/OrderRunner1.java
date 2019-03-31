3bcc9dd0f30fd39b6accea45087cfec9286918ad
mistyle <aaplp@qq.com>
2019-03-31 08:35:08 +0800

    update
    (cherry picked from commit 6584287b8ddcdcb166a4c94a6ef71001d91cab65)

--- src/main/java/com/example/demo/test/commandlinerunner/OrderRunner1.java
+++ src/main/java/com/example/demo/test/commandlinerunner/OrderRunner1.java
@@ -1,14 +1,14 @@
-package com.example.demo.test.commandlinerunner;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-@Component
-@Order(2)
-public class OrderRunner1 implements CommandLineRunner {
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("The OrderRunner1 start to initialize ...");
-    }
-}
+//package com.example.demo.test.commandlinerunner;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Order(2)
+//public class OrderRunner1 implements CommandLineRunner {
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("The OrderRunner1 start to initialize ...");
+//    }
+//}
