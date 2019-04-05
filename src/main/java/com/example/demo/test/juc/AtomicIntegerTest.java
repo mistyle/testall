de9fc13692d59936c5d17ea46d215a5b1fba7d72
mistyle <aaplp@qq.com>
2019-04-05 22:32:32 +0800

    dcpcall修改好

--- src/main/java/com/example/demo/test/juc/AtomicIntegerTest.java
+++ src/main/java/com/example/demo/test/juc/AtomicIntegerTest.java
@@ -1,14 +1,8 @@
 package com.example.demo.test.juc;
 
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicIntegerTest {
@@ -21,86 +15,85 @@ public class AtomicIntegerTest {
 //		 executor.execute(new Thread() {
 //				@Override
 //				public void run() {
-		
-		//子线程的阻塞，只会影响父线程内部，而不会阻塞到主线程，也就是不能越级阻塞
+
+
 		 
 		String jobID = "REQUEST_SUBSCRIPTION_STATUS_CHANGE";
 		Map<String, Future> futures = new HashMap<>();
 		final AtomicInteger count = new AtomicInteger(0);
-		final CountDownLatch countDownLatch = new CountDownLatch(1);
-		ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-		Future future = scheduler.scheduleWithFixedDelay(new Runnable() {
+		final CountDownLatch countDownLatch = new CountDownLatch(1); //如果countDown()执行次数<这里参数,会导致死锁，如果执行次数大于参数，以conntdown执行次数为准
+		ScheduledExecutorService scheduledExcutor = Executors.newSingleThreadScheduledExecutor();
+		Future future = scheduledExcutor.scheduleWithFixedDelay(new Runnable() {
 
 			@Override
 			public void run() {
-				System.out.println("ddddddddd");
-				
-				for(int i=0;i<10;i++){
-					String a=i*133/334+23+"xxx";
-					System.out.println(a);
+				System.out.println(count.getAndIncrement());
+
+				Future future = futures.get(jobID);
+				String state = "In progress";
+				if ("completed".equals(state) || "canceld".equals(state) || "Rejected".equals(state)) {
+
+					if (future != null) {
+						future.cancel(true);
+
+					}
+					countDownLatch.countDown(); // 开始这里没写这一句，导致子线程挂起，主线程得不到执行
+
+				} else if ("In progress".equals(state)) {
+					if (count.get() > 2) {
+
+						if (future != null) {
+							future.cancel(true);
+						}
+						countDownLatch.countDown();
+					}
+
 				}
-				System.out.println("sssss");
-				
-//				count.getAndIncrement();
-//				//
-//				Future future = futures.get(jobID);
-//				// if (count.get() >2) {
-//				// System.out.println("sss " +count.get());
-//				// if (future != null) future.cancel(true);
-//				// countDownLatch.countDown();
-//				// }
-//				//
-//
-//				
-//			
-//				
-//				// dcp
-//				// //(1) 调查询接口查状态
-//				String state = "complated";
-//				if ("complated".equals(state) || "canceld".equals(state) || "Rejected".equals(state)) {
-//					if (future != null) {
-//						future.cancel(true);
-//
-//					}
-//					countDownLatch.countDown(); // 开始这里没写这一句，导致子线程挂起，主线程得不到执行
-//
-//				} else if ("In progress".equals(state)) {
-//					if (count.get() > 2) {
-//
-//						if (future != null) {
-//							future.cancel(true);
-//						}
-//						countDownLatch.countDown();
-//					}
-//
-//				}
+
+
 
 			}
 		}, 0, 1, TimeUnit.SECONDS);
 
 		futures.put(jobID, future);
+
+		countDownLatch.await();
+		// scheduledExcutor.shutdown();
+
 		try {
-			countDownLatch.await();
+			scheduledExcutor.shutdown();
+			// (所有的任务都结束的时候，返回TRUE)
+			if (!scheduledExcutor.awaitTermination(5, TimeUnit.MINUTES)) {
+				// 超时的时候向线程池中所有的线程发出中断(interrupted)。
+				scheduledExcutor.shutdownNow();
+			}
 		} catch (InterruptedException e) {
-			// Restore the interrupted status
-			Thread.currentThread().interrupt();
+			//logger.error("awaitTermination interrupted: " + e);
+			scheduledExcutor.shutdownNow();
 		}
 
-		//scheduler.shutdown();
-				
 		 
-		 //如果要让最后的打印先执行，必须再调度任务上再包装成线程执行
-		System.out.println("sssssssssssssssssssssssssssssssssss"); // 注意子线程 不停止，这里打印得不到执行
+
+		System.out.println("end........");
 		
 		
 
 	}
 
 }
+/**
+        0
+		1
+		2
+		3
+		4
+		5
+		6
+		7
+		8
+		9
+		10
+		sssssssssssssssssssssssssssssssssss
 
-// 0
-// hello world!
-// 1
-// hello world!
-// 2
+		Process finished with exit code 0  **/
 
