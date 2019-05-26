7f19ef39cab816d3c4f04b97badcb52126432a97
mistyle <aaplp@qq.com>
2019-05-26 17:46:40 +0800

    future

---
+++ src/main/java/com/example/demo/test/parametertype/GenericMethod.java
@@ -0,0 +1,26 @@
+package com.example.demo.test.parametertype;
+
+/**
+ * @program: testall
+ * @Date: 2019/5/23 22:56
+ * @Author: mistyle
+ * @Description:
+ */
+public class GenericMethod {
+
+	public static void main   (String[] args)
+
+	{
+
+		String s = "hello!Nice to meet you!";
+
+		Integer inter = 3;
+
+		Show show = new Show();
+
+		show.print(s);
+
+		show.print(inter);
+
+	}
+}
---
+++ src/main/java/com/example/demo/test/parametertype/Show.java
@@ -0,0 +1,18 @@
+package com.example.demo.test.parametertype;
+
+/**
+ * @program: testall
+ * @Date: 2019/5/23 22:58
+ * @Author: mistyle
+ * @Description:
+ */
+public class Show {
+
+   public <Q> void print(Q q)
+
+			 {
+
+   System.out.println(q);
+
+   }
+}
